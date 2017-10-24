54ca8f930e1b1dedd7e7e277daf93638471f7393
Begum.Yazici <yazicibegum@gmail.com>
2017-10-24 13:54:22 +0300

    sms sending is commited

--- MyApplication/app/src/main/java/intentclass/begi/com/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/intentclass/begi/com/myapplication/MainActivity.java
@@ -1,13 +1,36 @@
 package intentclass.begi.com.myapplication;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button sendSMS,sendMAIL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sendSMS = (Button) findViewById(R.id.btn_sendSms);
+        sendMAIL = (Button) findViewById(R.id.btn_sendMail);
+
+        sendSMS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String smsNumber = "05535040000";
+                String smsText = "Hello, I'm a test message!";
+                Uri uri = Uri.parse("smsto:"+ smsNumber);
+                Intent intent = new Intent(Intent.ACTION_SENDTO,uri);
+                intent.putExtra("sms_body",smsText);
+                startActivity(intent);
+            }
+        });
+
     }
 }
